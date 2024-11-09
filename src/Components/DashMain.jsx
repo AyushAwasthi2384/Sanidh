2f8613cc69c7a7d8bd7f233d0c0666d49e5043a1
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2024-11-09 17:53:55 +0530

    session count

--- src/Components/DashMain.jsx
+++ src/Components/DashMain.jsx
@@ -11,7 +11,18 @@ const DashMain = () => (
             <div className='bg-white rounded-3xl w-3/4'></div>
             <div className='bg-white rounded-3xl w-3/4'></div>
             <div className='flex flex-col gap-[1rem] rounded-3xl w-1/4'>
-                <div className='bg-white h-2/3 rounded-3xl w-full'></div>
+
+                <div className='bg-white h-2/3 rounded-3xl w-full'>
+                    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                        <div className="flex justify-center items-baseline" style={{}}>
+                            <div className="text-6xl font-bold text-blue-500">1</div>
+                            <div className="text-6xl font-bold text-gray-500 mx-1">/</div>
+                            <div className="text-6xl font-bold text-gray-500">3</div>
+                        </div>
+                        <div className="mt-4 text-base font-medium whitespace-nowrap" style={{fontSize:'0.9rem', fontWeight:'bold'}}>Free Sessions Has Been Used</div>
+                    </div>
+                </div>
+
                 <div className='bg-white h-1/3 rounded-3xl w-full'></div>
             </div>
         </div>
