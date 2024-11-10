4d5d92afd94a8f4e0e985612dab7c80b2660a08d
Alok Pratap Verma <alok12verma903@gmail.com>
2024-11-10 07:31:34 +0530

    added the chat

--- src/Components/SessionMain.jsx
+++ src/Components/SessionMain.jsx
@@ -11,12 +11,12 @@ const SessionMain = () => {
         <TopNavbar route={"Session"} userName={"Ayush Awasthi"} />
       </div>
       <div className="flex gap-[1rem] items-center h-full bg-white rounded-3xl">
-        <div
+        {/* <div
           className=" flex flex-col  gap-[3rem] "
           style={{ width: "100%", textAlign: "center" }}
         >
           <div className="flex flex-col gap-[0rem]">
-            <div className="w-92% flex mb-[-2rem] justify-center">
+            <div className="w-92% flex mb-[-1rem] justify-center">
               <img src="/images/logo.png" alt="logo" height={200} width={200} />
             </div>
             <div>
@@ -38,11 +38,6 @@ const SessionMain = () => {
               </h4>
             </div>
           </div>
-          {/* <div className={'flex flex-col gap-[1rem] w-[70%]'}>
-                    <div className='bg-yellow-300 h-[30%] rounded-3xl relative'>
-                        <Image src="/images/Frame 2609065.png" className="z-10 w-full h-full absolute object-cover rounded-3xl" alt="Logo" width={1800} height={700} />
-                    </div>
-                </div> */}
           <div className="container  ">
             <div
               className=" rounded-3xl w-[50%] h-[7rem] p-4 px-5 mx-auto flex justify-between items-center"
@@ -50,20 +45,12 @@ const SessionMain = () => {
                 backgroundImage: `url('/images/sessionmain.png')`,
               }}
             >
-              {/* <Image
-                src="/images/sessionmain.png"
-                className="absolute w-full h-full object-cover rounded-3xl"
-                alt="Logo"
-                width={1800}
-                height={500}
-              /> */}
               <div
                 className="font-bold text-[1.3rem]"
                 style={{ padding: "15px" }}
               >
                 Upload Your First Document
               </div>
-              {/* <div className='w-[70%]'>aaaa</div> */}
 
               <div className="flex flex-col gap-3  ">
                 <button
@@ -82,6 +69,18 @@ const SessionMain = () => {
               </div>
             </div>
           </div>
+        </div> */}
+        <div className="w-full min-h-24  px-16  ">
+          <UserChat
+            message={
+              " giving the ofx in the various way to  response to the system goes to the various team name allowed o the fox generated the meaage to tgo the to pibn tet"
+            }
+          />
+          <AIResponse
+            message={
+              " giving the ofx in the various way to  response to the system goes to the various team name allowed o the fox generated the meaage to tgo the to pibn tet"
+            }
+          />
         </div>
         <div className="w-[92%] flex justify-center fixed  bottom-[2rem] mx-auto">
           <div className="w-full flex justify-center  ">
@@ -113,3 +112,36 @@ const SessionMain = () => {
   );
 };
 export default SessionMain;
+
+const UserChat = ({ message }) => {
+  return (
+    <div className="flex flex-row-reverse gap-2 my-[1rem]">
+      <div
+        className="w-10 h-10 bg-gray-300 rounded-full object-cover object-center border border-gray-500 "
+        style={{
+          backgroundImage: `url('/images/india-flag.png')`,
+        }}
+      ></div>
+      <div className=" p-2 px-4 rounded-2xl bg-[#F2F6FE] max-w-[60%]">
+        {message}
+      </div>
+    </div>
+  );
+};
+const AIResponse = ({ message }) => {
+  return (
+    <div className="flex gap-2 px-16 my-[1rem]">
+      <div
+        className="w-10 h-10 bg-[#CDE8F6] ] rounded-full object-cover object-center border border-gray-500 overflow-hidden "
+        // style={{
+        //   backgroundImage: `url('/images/logo.png')`,
+        // }}
+      >
+        <img src="images/logo.png" alt="logo" width={80} height={80} />
+      </div>
+      <div className=" p-2 px-3 rounded-2xl bg-[#CDE8F6] max-w-[60%]">
+        {message}
+      </div>
+    </div>
+  );
+};
